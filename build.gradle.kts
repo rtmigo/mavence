2d6bffa599b8f4ac0561c84d93e835e9552410dd
Artem IG <ortemeo@gmail.com>
2022-10-20 21:24:49 +0300

    upd

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0") // testing
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0") // testing
-    testImplementation("io.kotest:kotest-assertions-core:5.5.1")// testing
+    testImplementation("io.kotest:kotest-assertions-core:5.5.1") // testing
 
     implementation("com.github.pgreze:kotlin-process:1.4") // running processes
     implementation("com.github.aballano:mnemonik:2.1.1") // caching function results
